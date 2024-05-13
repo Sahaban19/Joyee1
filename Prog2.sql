2d95abbd922045e86f37239c09a646bc985b418f
Sahana <108896491+Sahaban19@users.noreply.github.com>
2024-05-13 13:46:10 +0530

    Create Prog2.sql

---
+++ Prog2.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Employee (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone_number VARCHAR(20),
+    hire_date DATE,
+    job_title VARCHAR(100),
+    department VARCHAR(100),
+    salary DECIMAL(10, 2),
+    manager_id INT
+);
